d7a026c9e49af7f9ecfc4d39b718b4f69096fc17
Tim Gorton <tgorton@zeus.health>
2020-12-28 02:48:57 +0000

    Add dsnet's solution

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # protoc-gen-go-option-example
-Demonstration of how to use custom options in a golang compiler/protogen protoc plugin
+
+Demonstration of how to use custom options in a golang compiler/protogen protoc plugin using the solution provided by @dsnet at [golang/protobuf issue # 1260](https://github.com/golang/protobuf/issues/1260). Thanks @dsnet!
+
+## How to run
+
+First you need protoc installed (and, of course, go.)
+
+Then run:
+
+`make`
+
+The output (in ./generatedresult/out.txt) should look like:
+
+```text
+
+Message Foo:
+Value of option example_annotation_int32 is 1234
+Value of option example_annotation_string is hello
+
+Message Bar:
+Value of option example_annotation_string is world
+```
--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"google.golang.org/protobuf/compiler/protogen"
-	"google.golang.org/protobuf/types/pluginpb"	
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/dynamicpb"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
@@ -12,6 +18,16 @@ func main() {
 
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 
+		// The type information for all extensions is in the source files,
+		// so we need to extract them into a dynamically created protoregistry.Types.
+		extTypes := new(protoregistry.Types)
+		for _, file := range gen.Files {
+			if err := registerAllExtensions(extTypes, file.Desc); err != nil {
+				panic(err)
+			}
+		}
+
+		// run through the files again, extracting and printing the Message options
 		for _, sourceFile := range gen.Files {
 			if !sourceFile.Generate {
 				continue
@@ -19,15 +35,64 @@ func main() {
 
 			// setup output file
 			outputfile := gen.NewGeneratedFile("./out.txt", sourceFile.GoImportPath)
-			
-			// pull out the particular message we know is in the file for this example
-			options := sourceFile.Messages[0].Desc.Options()
 
-			// how to get the custom options values out of the 'options' protoreflect.ProtoMessage?
-			// just print the value to prove they are in there
-			outputfile.P(fmt.Sprintf("Options value is:%v",options))
+			for _, message := range sourceFile.Messages {
+				outputfile.P(fmt.Sprintf("\nMessage %s:", message.Desc.Name()))
 
+				// The MessageOptions as provided by protoc does not know about
+				// dynamically created extensions, so they are left as unknown fields.
+				// We round-trip marshal and unmarshal the options with
+				// a dynamically created resolver that does know about extensions at runtime.
+				options := message.Desc.Options().(*descriptorpb.MessageOptions)
+				b, err := proto.Marshal(options)
+				if err != nil {
+					panic(err)
+				}
+				options.Reset()
+				err = proto.UnmarshalOptions{Resolver: extTypes}.Unmarshal(b, options)
+				if err != nil {
+					panic(err)
+				}
+
+				// Use protobuf reflection to iterate over all the extension fields,
+				// looking for the ones that we are interested in.
+				options.ProtoReflect().Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+					if !fd.IsExtension() {
+						return true
+					}
+
+					outputfile.P(fmt.Sprintf("Value of option %s is %s",fd.Name(), v.String()))
+
+					// Make use of fd and v based on their reflective properties.
+
+					return true
+				})
+			}
 		}
+		
 		return nil
 	})
+}
+
+// Recursively register all extensions into the provided protoregistry.Types,
+// starting with the protoreflect.FileDescriptor and recursing into its MessageDescriptors,
+// their nested MessageDescriptors, and so on.
+//
+// This leverages the fact that both protoreflect.FileDescriptor and protoreflect.MessageDescriptor 
+// have identical Messages() and Extensions() functions in order to recurse through a single function
+func registerAllExtensions(extTypes *protoregistry.Types, descs interface {
+	Messages() protoreflect.MessageDescriptors
+	Extensions() protoreflect.ExtensionDescriptors
+}) error {
+	mds := descs.Messages()
+	for i := 0; i < mds.Len(); i++ {
+		registerAllExtensions(extTypes, mds.Get(i))
+	}
+	xds := descs.Extensions()
+	for i := 0; i < xds.Len(); i++ {
+		if err := extTypes.RegisterExtension(dynamicpb.NewExtensionType(xds.Get(i))); err != nil {
+			return err
+		}
+	}
+	return nil
 }
